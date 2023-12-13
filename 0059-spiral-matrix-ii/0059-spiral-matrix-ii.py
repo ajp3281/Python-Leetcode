65ded942fe0b8975383fc4643bde586feefc0f7a
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2023-12-13 17:36:04 -0500

    Time: 35 ms (82.06%), Space: 16.5 MB (5.33%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -13,8 +13,6 @@ class Solution:
             
             #build top row
             for i in range(colstart,colend):
-                if count >= n*n:
-                    return matrix
                 matrix[rowstart][i] = count+1 
                 count += 1
             rowstart += 1
@@ -22,24 +20,18 @@ class Solution:
     
             # build rightmost row
             for i in range(rowstart,rowend):
-                if count >= n*n:
-                    return matrix
                 matrix[i][colend-1] = count+1
                 count += 1
             colend -= 1
 
             # build bottom row
             for i in reversed(range(colstart,colend)):
-                if count >= n*n:
-                    return matrix
                 matrix[rowend-1][i] = count+1
                 count += 1
             rowend -= 1
             
             # built leftmost col
             for i in reversed(range(rowstart,rowend)):
-                if count >= n*n:
-                    return matrix
                 matrix[i][colstart] = count+1
                 count += 1
             colstart += 1
