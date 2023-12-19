fa7e18a631954c4818d7440057e80298c39d6bb8
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2023-12-19 15:23:59 -0500

    Time: 297 ms (50.69%), Space: 27.9 MB (7.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         visited = set()
-        def bfs(r, c):
-            q = deque()
-            q.append((r,c))
+        def dfs(r, c):
             
-            while q:
-                currentrow, currentcol = q[0]
-                q.popleft()
-                visited.add((currentrow,currentcol))
+            if ((r,c) in visited or grid[r][c] == "0"):
+                return
+            
+            visited.add((r,c))
+            
+            if (r + 1 < len(grid)):
+                dfs(r+1,c)
+            if r-1 >= 0:
+                dfs(r-1,c)
+            if c+1 < len(grid[0]):
+                dfs(r,c+1)
+            if c-1 >= 0:
+                dfs(r,c-1)
                 
-                if (currentrow+1 < len(grid) and grid[currentrow+1][currentcol] == "1" and (currentrow+1,currentcol) not in visited):
-                    q.append((currentrow+1,currentcol))
-                    visited.add((currentrow+1,currentcol))
-                    
-                if (currentrow-1 >= 0 and grid[currentrow-1][currentcol] == "1" and (currentrow-1,currentcol) not in visited):
-                    q.append((currentrow-1,currentcol))
-                    visited.add((currentrow-1,currentcol))
-                    
-                if (currentcol+1 < len(grid[0]) and grid[currentrow][currentcol+1] == "1" and (currentrow,currentcol+1) not in visited):
-                    q.append((currentrow,currentcol+1))
-                    visited.add((currentrow,currentcol+1))
-                    
-                if (currentcol-1 >= 0 and grid[currentrow][currentcol-1] == "1" and (currentrow,currentcol-1) not in visited):
-                    q.append((currentrow,currentcol-1))
-                    visited.add((currentrow,currentcol-1))
-                    
-                
-                    
-                
-        
         
         
         res = 0
@@ -38,7 +25,9 @@ class Solution:
         for i in range(r):
             for j in range(c):
                 if (i,j) not in visited and grid[i][j] == "1":
-                    bfs(i,j)
+                    print(i,j)
+                    dfs(i,j)
+                    #visited.add((i,j))
                     res += 1
                     
          
