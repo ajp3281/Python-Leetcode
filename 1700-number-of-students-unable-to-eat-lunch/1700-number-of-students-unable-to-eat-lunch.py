46c1810ff65095523f47c60cd7e9970e57817e6f
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-01-07 15:29:41 -0500

    Time: 41 ms (76.71%), Space: 17.5 MB (12.45%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,4 +1,7 @@
+# o(n^2) time using queue - worst case need to check every student every time you reach new sandwich - can be improved by not using queue
+# o(n) space - o(2n) because 2 queues but rounds down to o(n) - can be done in o(1) without using queue
 from collections import deque
+
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
