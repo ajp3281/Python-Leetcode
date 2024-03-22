2f3c5c0aac673bb9c0271ba1a54b1606601e10fb
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-03-22 14:20:13 -0500

    Time: 88 ms (63.86%), Space: 20.3 MB (48.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-        for str in strs:
-            key = tuple(sorted(str))
-            anagrams[key].append(str)
         
-        return list(anagrams.values())
+        # sort all the words and use as key for hashmap - 
+        stringmap = defaultdict(list)
+        for word in strs:
+            sortedword = ''.join(sorted(word))
+            stringmap[sortedword].append(word)
+            
+            
+        print(stringmap)
+        
+        res = []
+        for key in stringmap.keys():
+            res.append(stringmap[key])
+        
+        return res
+            
+            
+            
             
         
