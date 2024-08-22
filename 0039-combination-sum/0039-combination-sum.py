84108a4d39448c3366c66e77ba6b93e65904ed54
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-08-22 13:52:52 -0400

    Time: 38 ms (98.35%), Space: 16.6 MB (73.06%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,26 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # 
+        
+        def helper(index, current, count):
+            
+            if count == target:
+                res.append(current.copy())
+                return
+            
+            if count > target or index >= len(candidates):
+                return
+            
+            # repeat same number
+            current.append(candidates[index])
+            helper(index, current, count + candidates[index])
+            # remove current and try next
+            current.pop()
+            helper(index + 1, current, count)
+        
+        
         res = []
-        subset = []
-        
-        def backtrack(index):
-            
-            if sum(subset) > target:
-                return
-            
-            if sum(subset) == target:
-                res.append(subset.copy())
-                return
-            
-            for i in range(index,len(candidates)):
-                
-                subset.append(candidates[i])
-                backtrack(i)
-                subset.pop()
-            
-        backtrack(0)
+        helper(0, [], 0)
         return res
-            
-            
-        
