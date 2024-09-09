fbca7b7a882db9c1da70e73c1cf55f54d39bb4ed
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-09-09 16:50:15 -0400

    Time: 32 ms (87.37%), Space: 16.9 MB (51.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,38 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # recursion
-        # add set of parentheses at back?
-        # add parantheses around each exising pair
-        def backtrack(s='', opencount=0, closedcount=0):
-            if len(s) == 2*n and opencount == closedcount:
-                res.append(s)
-            if opencount < n:
-                backtrack(s+'(', opencount + 1, closedcount)
-            if closedcount < opencount:
-                backtrack(s+')', opencount, closedcount+1)
-                
+        
+        '''
+        
+        dp for sure
+        if left count == right count : need left
+        if left count < right count : can do either
+        if left count == n and right < n : need right
+        if left and right == n : valid case
+        
+        pseudo code
+        dfs helper
+        if left == n and right == n:
+            add current path
+        if left == n:
+            add right
+            
+        '''
         res = []
-        stck = []
-        backtrack()
+        def dfs(left, right, path):
+            if right > left or left > n or right > n:
+                return
+            if left == n and right == n:
+                res.append(''.join(path.copy()))
+            if left < n:
+                path.append("(")
+                dfs(left + 1, right, path)
+                path.pop()
+            if right < n:
+                path.append(")")
+                dfs(left, right + 1, path)
+                path.pop()
+            return
+        
+        dfs(0,0,[])
         return res
-        
-        
-        
-        
         
