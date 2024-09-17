f2cd47dbee44367f7c650fa2a7e570009f0cde50
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-09-17 12:44:29 -0400

    Time: 81 ms (90.32%), Space: 21.3 MB (44.80%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,35 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #store freq of every val in hashmap
-        #new hashmap with <key,array<Val>> to store all by freq
-        #reverse the hashmap keys to access from back
-        #keep pushing until count reached
         
-        myMap = defaultdict(int)
-        
-        for num in nums:
-            myMap[num] += 1
-            
-        matrix = [[] for i in range(len(nums)+1)]
-        
-        for number,value in myMap.items():
-            matrix[value].append(number)
-        
+        counter = Counter(nums)
+        arr = []
+        for elem in counter.keys():
+            arr.append((-counter[elem], elem))
+        heapq.heapify(arr)
         res = []
-        n = 0
-        
-        for row in range(len(matrix)-1,-1,-1):
-            for num in range(len(matrix[row])):
-                    res.append(matrix[row][num])
-                    n += 1
-                    if (n >= k):
-                        return res
+        for i in range(k):
+            res.append(heapq.heappop(arr)[1])
+        return res
             
-        
-        
-        
-        
-        
-        
 
-            
+        
+        
+        
