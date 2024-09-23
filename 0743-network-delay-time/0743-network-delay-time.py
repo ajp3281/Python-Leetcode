df8160279e46f3208ce4b5b481e9acd561cb14b1
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-09-23 11:09:51 -0400

    Time: 491 ms (7.28%), Space: 18.7 MB (43.75%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,36 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        
+        # 1 -> 2 - > 3
+        #.  1     2
+        # 1 ------> 3
+        #.     2
+        adj = defaultdict(list)
+        for time in times:
+            adj[time[0]].append((time[1], time[2]))
+        min_heap = [(0, k)]
+        visited = {}
+        
+        visited[k] = 0
+        heapq.heapify(min_heap)
+        
+        while min_heap:
+            current_cost, current_var = heapq.heappop(min_heap)
+            
+            if current_var not in visited:
+                visited[current_var] = current_cost
+            elif current_var in visited and visited[current_var] > current_cost:
+                visited[current_var] = current_cost
+            
+            
+            for neighbor, weight in adj[current_var]:
+                if neighbor not in visited:
+                    heapq.heappush(min_heap, ((current_cost + weight), neighbor))
+        
+
+        if len(visited) != n:
+            return -1
+        return max(visited.values())
+            
+            
+        
