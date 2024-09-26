33a87f52e6ab466a0de7d00e08dfbfd3f76521ed
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-09-26 18:51:40 -0400

    Time: 1409 ms (5.04%), Space: 17.4 MB (35.52%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,30 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.bookings = []
+
+        # ["MyCalendar","book","book","book","book","book","book","book","book","book","book"]
+#[[],[47,50],[33,41],[39,45],[33,42],[25,32],[26,35],[19,25],[3,8],[8,13],[18,27]]
+# 47,50 -> 33,50 -> 
+# make arr of intervals 
+    def book(self, start: int, end: int) -> bool:
+        if not self.bookings:
+            self.bookings.append((start, end))
+            return True
+        
+        sorted_intervals = sorted(self.bookings)
+        for interval in sorted_intervals:
+            if (start >= interval[0] and start < interval[1]) or (end > interval[0] and end <= interval[1]) or (start < interval[0] and end > interval[1]):
+                return False
+        
+        self.bookings.append((start, end))
+        return True
+            
+        
+        
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
