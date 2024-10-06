b7e35309b1834f4218f42c530c23465923abd3ec
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2024-10-06 16:18:12 -0400

    Time: 152 ms (5.16%), Space: 17 MB (19.03%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -0,0 +1,56 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = {}
+        self.rank = {}
+        for i in range(n):
+            self.par[i] = i
+            self.rank[i] = 0
+        
+    def find(self, n):
+        if self.par[n] == n:
+            return n
+        self.par[n] = self.find(self.par[n])
+        return self.par[n]
+    
+    def union(self, n1, n2):
+        par1 = self.find(n1)
+        par2 = self.find(n2)
+        
+        if par1 != par2:
+            if self.rank[par1] > self.rank[par2]:
+                self.par[par2] = par1
+            elif self.rank[par2] > self.rank[par1]:
+                self.par[par1] = par2
+            else:
+                self.par[par2] = par1
+                self.rank[par1] += 1
+    
+class Solution:
+    def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        
+        # union-find problem for sure
+        # add edge for log in logs
+        # union every edge
+        # check if all nodes share same parent and return timestamp if so
+        # if we get through all logs and not all same paretn return -1
+        uf = UnionFind(n)
+        logs.sort()
+        for log in logs:
+            time = log[0]
+            n1 = log[1]
+            n2 = log[2]
+            
+            uf.union(n1,n2)
+            boolean = True
+            first_found = uf.find(0)
+            for i in range(1,n):
+                if uf.find(i) != first_found:
+                    boolean = False
+            print(uf.par)
+            if boolean:      
+                return time
+                
+            
+        return -1
+        
+        
