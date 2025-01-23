2231a9b4a3dfb1df1e15a6597a59b0ea6d24a260
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-01-23 18:04:36 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,34 +1,29 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        
-        # 1 -> 2 - > 3
-        #.  1     2
-        # 1 ------> 3
-        #.     2
+
+        # djikstras, see how long it takes to visit every node
+        # keep track of costs and return max 
         adj = defaultdict(list)
+        costs = defaultdict(int)
         for time in times:
             adj[time[0]].append((time[1], time[2]))
-        min_heap = [(0, k)]
-        visited = {}
         
-    
-        heapq.heapify(min_heap)
-        
-        while min_heap:
-            current_cost, current_var = heapq.heappop(min_heap)
-            
-            if current_var in visited:
-                continue
-            visited[current_var] = current_cost
-            
-            for neighbor, weight in adj[current_var]:
-                if neighbor not in visited:
-                    heapq.heappush(min_heap, ((current_cost + weight), neighbor))
-        
+        heap = [(0,0,k)]
+        heapq.heapify(heap)
 
-        if len(visited) != n:
+        # how to avoid adding
+        while heap:
+            current_cost, dist, node = heapq.heappop(heap)
+
+            costs[node] = current_cost
+
+            for nei, nei_cost in adj[node]:
+                new_cost = current_cost + nei_cost
+                if nei not in costs or new_cost < costs[nei]:
+                    heapq.heappush(heap,(new_cost, dist + 1, nei))
+
+        if len(costs) != n:
             return -1
-        return max(visited.values())
-            
-            
-        
+        return max(costs.values())
+
+
