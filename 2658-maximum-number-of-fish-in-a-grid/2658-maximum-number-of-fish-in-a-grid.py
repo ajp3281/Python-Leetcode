c18a128ed1c3f3308fabda691dc12b75ffeb5889
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-01-27 19:59:54 -0500

    Time: 71 ms (14.88%), Space: 18 MB (49.02%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -4,21 +4,21 @@ class Solution:
         def in_bounds(row, col):
             return 0 <= row < len(grid) and 0 <= col < len(grid[0])
         
-        def dfs(row, col, visited):
-            
-            res = grid[row][col]
+        def bfs(row, col, visited):
             visited.add((row,col))
-            
-            directions = [(-1,0), (0,-1), (1,0), (0,1)]
-            
-            
-            for direction in directions:
-                new_row = row + direction[0]
-                new_col = col + direction[1]
-                
-                if (new_row, new_col) not in visited and in_bounds(new_row, new_col) and grid[new_row][new_col] > 0:
-                    res += dfs(new_row, new_col, visited)
-            
+            directions = [(1,0), (-1,0), (0,-1), (0,1)]
+
+            q = deque([(row,col)])   
+            res = 0
+            while q:
+                cur_row, cur_col = q.popleft()
+                res += grid[cur_row][cur_col]
+
+                for direction in directions:
+                    new_row, new_col = cur_row + direction[0], cur_col + direction[1]
+                    if in_bounds(new_row, new_col) and grid[new_row][new_col] > 0 and (new_row, new_col) not in visited:
+                        visited.add((new_row, new_col))
+                        q.append((new_row, new_col))
             return res
         
         result = 0
@@ -26,6 +26,6 @@ class Solution:
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if (row,col) not in visited and grid[row][col] != 0:
-                    result = max(result, dfs(row,col,visited)) 
+                    result = max(result, bfs(row,col,visited)) 
         return result
         
