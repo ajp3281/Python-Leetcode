31082cd193251ca9c5c45a2f906eae5433d3d589
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-02-05 18:15:57 -0500

    Time: 3 ms (54.28%), Space: 18.1 MB (73.52%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,52 +1,37 @@
 class UnionFind:
-    
     def __init__(self, n):
         self.par = {}
         self.rank = {}
-        
-        for i in range(1,n+1):
+        for i in range(1,n + 1):
             self.par[i] = i
             self.rank[i] = 0
-            
+    
     def find(self, n):
         if self.par[n] == n:
             return n
-        else:
-            return self.find(self.par[n])
-    
+        self.par[n] = self.find(self.par[n])
+        return self.par[n]
+
     def union(self, n1, n2):
-        parent1, parent2 = self.find(n1), self.find(n2)
-        
-        if parent1 == parent2:
+        par1, par2 = self.find(n1), self.find(n2)
+        if par1 == par2:
             return False
-        
-        if self.rank[parent1] > self.rank[parent2]:
-            self.par[parent2] = parent1
-        elif self.rank[parent2] > self.rank[parent1]:
-            self.par[parent1] = parent2
+        if self.rank[par1] > self.rank[par2]:
+            self.par[par2] = par1
+            self.rank[par1] += 1
+        elif self.rank[par2] > self.rank[par1]:
+            self.par[par1] = par2
+            self.rank[par2] += 1
         else:
-            self.par[parent2] = parent1
-            self.rank[parent1] += 1
+            self.par[par1] = par2
+            self.rank[par2] += 1
         return True
-        
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(len(edges))
 
-        adj = defaultdict(list)
         for edge in edges:
-            adj[edge[0]].append(edge[1])
-            adj[edge[1]].append(edge[0])
-            
-        maxval = len(edges)
-        
-        unionfind = UnionFind(maxval)
-        for edge in edges:
-            if not unionfind.union(edge[0], edge[1]):
+            if not uf.union(edge[0], edge[1]):
                 return edge
-        
         return -1
-        
-        
-        
-        
-        
