fdac0aa1fdd389005aeadd4cd0fb064674c97f04
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-02-06 16:14:22 -0500

    Time: 0 ms (100%), Space: 18 MB (33.01%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,40 +1,21 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-
-        def helper(index, current, hashmap):
-            if index >= len(nums):
-                if tuple(current) not in hashmap:
-                    res.append(current.copy())
-                return
-            
-            if tuple(current) in hashmap:
-                return
-                           
-            # how to handle duplicate subsets?
-            
-            # at every position you take or dont take
-            # take last number of dupe?
-            
-            # naive approach - build all subsets and filter by using sets?
-             
-            #take
-            print("before adding: ", current, index)
-            current.append(nums[index])
-            print("after adding: ", current, index)
-            helper(index + 1, current, hashmap)
-            
-            hashmap.add(tuple(current))
-
-            # skip
-            print("before removal: ", current, index)
-            current.pop()
-            print("after removal: ", current, index)
-            helper(index + 1, current, hashmap)
-
-        
-        res = []
         nums.sort()
-        hashmap = set()
-        helper(0, [], hashmap)
-        return res
+        # only start from unique vals
+        def backtrack(i,path):
+            if i >= len(nums):
+                subsets.append(path.copy())
+                return
+
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+            # skip path will not include any dupes!
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, path)
+
+        subsets = []
+        backtrack(0, [])
+        return subsets
         
