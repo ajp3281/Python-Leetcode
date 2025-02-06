223bfca08b0339915eb95a9f279999088e206674
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-02-06 16:39:33 -0500

    Time: 151 ms (32.42%), Space: 59.8 MB (16.61%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,22 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        res = []
-        def helper(i, path):
-            if len(path) == k:
-                res.append(path.copy())
-                return 
-            if i > n:
-                return
-            
-            #for i in range(1,n+1)
-            if i not in path:
-                path.append(i)
-                take = helper(i + 1, path)
-                path.pop()
-            skip = helper(i + 1, path)
-            return
-
-        helper(1,[])
-        return res
         
+        combinations = []
+        def backtrack(i, path):
+            if len(path) == k:
+                combinations.append(path.copy())
+                return
+            if i > n or len(path) > k:
+                return
+
+            path.append(i)
+            backtrack(i + 1, path)
+            path.pop()
+            backtrack(i + 1, path)
+
+        backtrack(1, [])
+        return combinations
+
+
         
