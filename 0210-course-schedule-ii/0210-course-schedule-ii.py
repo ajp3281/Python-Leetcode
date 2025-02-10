b1933f703907cbc836826513a934102eb61ce9d4
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-02-10 16:52:55 -0500

    Time: 3 ms (78.31%), Space: 19 MB (83.34%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,38 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # topological sort algorithm
+        # if theres a cycle - we cant finish all the courses
+        # start connecting nodes starting from terminal ndoes
+        
+        in_deg = {}
         adj = defaultdict(list)
         for i in range(numCourses):
             adj[i] = []
+            in_deg[i] = 0
+
         for prereq in prerequisites:
-            adj[prereq[1]].append(prereq[0])
-            
-        in_deg = [0] * numCourses 
-        for edge in adj:
-            for node in adj[edge]:
-                in_deg[node] += 1
+            in_deg[prereq[1]] += 1
+            adj[prereq[0]].append(prereq[1])
 
-        
         q = deque([])
-        for i in range(len(in_deg)):
-            if in_deg[i] == 0:
-                q.append(i)
+        for node in in_deg.keys():
+            if in_deg[node] == 0:
+                q.append(node)
 
-        res = []       
+        topsort = []
         while q:
             current = q.popleft()
-            res.append(current)
-            
+            topsort.append(current)
+
             for nei in adj[current]:
                 in_deg[nei] -= 1
                 if in_deg[nei] == 0:
                     q.append(nei)
         
-        if len(res) == numCourses:
-            return res
-        else:
-            return []
-        
-                
-        
-            
-        
+        return list(reversed(topsort)) if len(topsort) == numCourses else []
