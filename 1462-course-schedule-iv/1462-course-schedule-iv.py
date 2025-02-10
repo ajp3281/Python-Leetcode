6f613a82e2ca848befaf40dd3401df9d7087210a
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-02-10 17:20:27 -0500

    Time: 81 ms (52.34%), Space: 20.6 MB (25.16%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,36 +1,48 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        
-        # brute force by doing bfs/dfs from nodes
-        adj = defaultdict(list)
+
+        # topological sort?
+        # how can we use it to make query time faster?
+        # brute force is just bfs/dfs for each query and see if we can complete it
+        # we should be flattening all prereqs..
+        # can we do this with topological sort?
+        # when we add to resulting array, include everything we have already visited as a prereq
+        # but if its disconnected all of them might not be necessary..
+        # well if its not in the list, must not be necessary
+        # if it is then it is?
+
+        in_deg = {}
+        adj = {}
+        for i in range(numCourses):
+            in_deg[i] = 0
+            adj[i] = []
+
         for prereq in prerequisites:
+            in_deg[prereq[1]] += 1
             adj[prereq[0]].append(prereq[1])
 
-        def bfs(i):
-            q = deque([i])
-            visited = set()
-            visited.add(i)
-            while q:
-                current = q.popleft()
-                for nei in adj[current]:
-                    if nei not in visited:
-                        q.append(nei)
-                        visited.add(nei)
-            return visited
+        q = deque([])
+        for node in in_deg.keys():
+            if in_deg[node] == 0:
+                q.append(node)
 
-        reachable = []
-        for i in range(numCourses):
-            visited = bfs(i)
-            reachable.append(visited)
+        isprereq = defaultdict(set)
+
+        while q:
+            node = q.popleft()
+
+            for nei in adj[node]:
+                in_deg[nei] -= 1
+                isprereq[nei].add(node)
+                for item in isprereq[node]:
+                    isprereq[nei].add(item)
+                if in_deg[nei] == 0:
+                    q.append(nei)
 
         res = []
         for query in queries:
-            if query[1] in reachable[query[0]]:
+            if query[0] in isprereq[query[1]]:
                 res.append(True)
             else:
                 res.append(False)
         return res
-
-        
-
-        
