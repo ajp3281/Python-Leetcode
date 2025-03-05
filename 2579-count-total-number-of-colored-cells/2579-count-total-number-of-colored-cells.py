8db80d834e15344d077bbe845178826a88566c2f
Alexander Pacheco <38512507+ajp3281@users.noreply.github.com>
2025-03-04 19:28:39 -0500

    Time: 149 ms (24.81%), Space: 17.7 MB (75.19%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.py
@@ -1,26 +1,7 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        '''
-        # brute force is bfs from border and return len(q) at minute
-        directions = [(-1,0), (0,-1), (1,0), (0,1)]
-        q = deque([(0, 0,0)])
 
-        visited = set()
-        cur_minute = 0
-        while cur_minute < n:
-            cur_minute, row, col = q.popleft()
-            visited.add((row, col))
-
-            for direction in directions:
-                new_row, new_col = row + direction[0], col + direction[1]
-
-                if (new_row, new_col) not in visited: 
-                    q.append((cur_minute + 1, new_row, new_col))
         
-        print(visited)
-        print(q)
-        return len(visited)
-        '''
 
         # check how many neighbors we add at every turn
         
